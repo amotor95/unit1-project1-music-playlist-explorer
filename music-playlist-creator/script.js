e38170a4e72fc0d88c25b4e1dcfc7aac3108a4a1
Jack McClure <jackmccl@meta.com>
2025-06-03 22:53:45 -0700

    Finished HTML and basic CSS for modal and playlists view - next add JS

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,12 @@
+const playlists_url = "/music-playlist-creator/data/data.json"
 
+const get_playlists = () => {
+    fetch(URL=playlists_url)
+    .then((data) => {
+        console.log(data)
+        return data
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
